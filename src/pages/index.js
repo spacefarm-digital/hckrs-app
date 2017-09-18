0bb54ededbf4d8559c3157677bb310d25e531f2f
Darin Dimitroff <d@thecrazyones.agency>
2017-09-18 18:06:09 +0300

    WIP

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,15 +3,19 @@
 import React, { Component } from 'react'
 import withRoot from '../components/withRoot'
 
+import ProfilePreview from '../components/ProfilePreview'
 import ChallengePreview from '../components/ChallengePreview'
 
-import { Container } from 'rebass'
+import { Container, Box } from 'rebass'
 
 class Index extends Component {
   render() {
     return (
       <Container pt={4}>
-        <ChallengePreview title="Lizard" description="Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica" />
+        <Box mb={3}>
+          <ProfilePreview />
+        </Box>
+        <ChallengePreview />
       </Container>
     )
   }
