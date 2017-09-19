ff8ef2577d86a387ad4bd41230b433ed4294ecbb
kalina-ivanova <k@spacefarm.digital>
2017-09-19 17:14:00 +0300

    Theming setup

--- src/components/withRoot.js
+++ src/components/withRoot.js
@@ -1,50 +1,51 @@
 /* eslint-disable flowtype/require-valid-file-annotation */
 
-import React, { Component } from 'react'
-import JssProvider from 'react-jss/lib/JssProvider'
-import { withStyles, MuiThemeProvider } from 'material-ui/styles'
-import wrapDisplayName from 'recompose/wrapDisplayName'
-import createContext from '../styles/createContext'
+import React, { Component } from "react";
+import JssProvider from "react-jss/lib/JssProvider";
+import { withStyles, MuiThemeProvider } from "material-ui/styles";
+import wrapDisplayName from "recompose/wrapDisplayName";
+import createContext from "../styles/createContext";
 
-import { Provider } from 'rebass'
+import { Provider } from "rebass";
 
 // Apply some reset
 const styles = theme => ({
-  '@global': {
+  "@global": {
     html: {
       background: theme.palette.background.default,
-      WebkitFontSmoothing: 'antialiased', // Antialiasing.
-      MozOsxFontSmoothing: 'grayscale', // Antialiasing.
+      WebkitFontSmoothing: "antialiased", // Antialiasing.
+      MozOsxFontSmoothing: "grayscale" // Antialiasing.
     },
     body: {
-      margin: 0,
-    },
-  },
-})
+      margin: 0
+    }
+  }
+});
 
-let AppWrapper = props => props.children
+let AppWrapper = props => props.children;
 
-AppWrapper = withStyles(styles)(AppWrapper)
+AppWrapper = withStyles(styles)(AppWrapper);
 
-const context = createContext()
+const context = createContext();
 
 function withRoot(BaseComponent) {
   class WithRoot extends Component {
     componentDidMount() {
       // Remove the server-side injected CSS.
-      const jssStyles = document.querySelector('#jss-server-side')
+      const jssStyles = document.querySelector("#jss-server-side");
       if (jssStyles && jssStyles.parentNode) {
-        jssStyles.parentNode.removeChild(jssStyles)
+        jssStyles.parentNode.removeChild(jssStyles);
       }
     }
 
     render() {
       return (
         <JssProvider registry={context.sheetsRegistry} jss={context.jss}>
+          {/* Rebass theme */}
           <Provider
             theme={{
-              font: 'Roboto, Helvetica, sans-serif',
-              fontSizes: [12, 16, 24, 36, 48, 72],
+              font: "Roboto, Helvetica, sans-serif",
+              fontSizes: [12, 16, 24, 36, 48, 72]
             }}
           >
             <MuiThemeProvider
@@ -57,15 +58,15 @@ function withRoot(BaseComponent) {
             </MuiThemeProvider>
           </Provider>
         </JssProvider>
-      )
+      );
     }
   }
 
-  if (process.env.NODE_ENV !== 'production') {
-    WithRoot.displayName = wrapDisplayName(BaseComponent, 'withRoot')
+  if (process.env.NODE_ENV !== "production") {
+    WithRoot.displayName = wrapDisplayName(BaseComponent, "withRoot");
   }
 
-  return WithRoot
+  return WithRoot;
 }
 
-export default withRoot
+export default withRoot;
