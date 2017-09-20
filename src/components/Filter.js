7d614534f992741d6f27af0c5ce0e7c4a5b1297e
Darin Dimitroff <d@thecrazyones.agency>
2017-09-20 19:57:47 +0300

    Test filter

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -7,27 +7,29 @@ import Paper from 'material-ui/Paper'
 import FilterItem from './FilterItem'
 
 const Filter = () => (
-  <Paper component={Box} px={3} mb={4} color="primary">
-    <Flex wrap align="baseline" f={1} py={2} mx={-2} px={2}>
-      <Text px={2} bold>
-        Showing
-      </Text>
-      <Box py={2} px={2}>
-        <FilterItem />
-      </Box>
-      <Text px={2} bold>
-        events happening
-      </Text>
-      <Box py={2} px={2}>
-        <FilterItem />
-      </Box>
-      <Text px={2} bold>
-        in
-      </Text>
-      <Box py={2} px={2}>
-        <FilterItem />
-      </Box>
-    </Flex>
+  <Paper color="primary">
+    <Box px={3} mb={4}>
+      <Flex wrap align="baseline" f={1} py={2} mx={-2} px={2}>
+        <Text px={2} bold>
+          Showing
+        </Text>
+        <Box py={2} px={2}>
+          <FilterItem />
+        </Box>
+        <Text px={2} bold>
+          events happening
+        </Text>
+        <Box py={2} px={2}>
+          <FilterItem />
+        </Box>
+        <Text px={2} bold>
+          in
+        </Text>
+        <Box py={2} px={2}>
+          <FilterItem />
+        </Box>
+      </Flex>
+    </Box>
   </Paper>
 )
 export default Filter
