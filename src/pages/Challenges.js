ad2a4996884f941213a819ebce0ab69f0b8b1630
Darin Dimitroff <d@thecrazyones.agency>
2017-09-20 19:58:18 +0300

    Test filter

--- src/pages/Challenges.js
+++ src/pages/Challenges.js
@@ -1,8 +1,16 @@
 /* eslint-disable flowtype/require-valid-file-annotation */
 
-import React, { Component } from 'react'
+import React, {
+	Component
+} from 'react'
 
-import { Container, Flex, Box, Text, Heading } from 'rebass'
+import {
+	Container,
+	Flex,
+	Box,
+	Text,
+	Heading
+} from 'rebass'
 
 import Paper from 'material-ui/Paper'
 import Grid from 'material-ui/Grid'
@@ -12,22 +20,23 @@ import Filter from '../components/Filter'
 import ChallengePreview from '../components/ChallengePreview'
 
 class Challenges extends Component {
-  render() {
-    return (
-      <div>
-        {/* <Filter /> */}
-        {/* Main */}
-        <Container is="main">
-          {/* <Grid container>
-            {[0, 1, 2].map(value => (
-              <Grid xs={12} sm={6} lg={4} xl={3} key={value} item>
-                <ChallengePreview />
-              </Grid>
-            ))}
-          </Grid> */}
-        </Container>
-      </div>
-    )
-  }
+	render() {
+		return ( <
+			div >
+			<
+			Filter / > { /* Main */ } <
+			Container is = "main" > {
+				/* <Grid container>
+				            {[0, 1, 2].map(value => (
+				              <Grid xs={12} sm={6} lg={4} xl={3} key={value} item>
+				                <ChallengePreview />
+				              </Grid>
+				            ))}
+				          </Grid> */
+			} <
+			/Container> <
+			/div>
+		)
+	}
 }
 export default Challenges
