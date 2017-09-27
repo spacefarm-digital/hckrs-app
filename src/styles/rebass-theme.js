46b37fce1ac3c09acf7facaaf7d304ebd9bccf5c
kalina-ivanova <k@spacefarm.digital>
2017-09-27 17:18:48 +0300

    Add Rebass theme
    Added the Rebass theme file and the colors.

---
+++ src/styles/rebass-theme.js
@@ -0,0 +1,60 @@
+// Default values
+const theme = {
+  breakpoints: [32, 48, 64, 80],
+  space: [0, 4, 8, 16, 32, 64, 128],
+  fontSizes: [12, 14, 16, 20, 24, 32, 48, 64, 72, 96],
+  weights: [400, 700],
+  colors: {
+    black: "#000",
+    white: "#fff",
+
+    // Primary
+    p50: "#FBEAE7",
+    p100: "#FFC7BC",
+    p200: "#FFA391",
+    p300: "#FF7F65",
+    p400: "#FF6243",
+    p500: "#FF4722",
+    p600: "#F4421E",
+    p700: "#D63717",
+    p800: "#C93214",
+    p900: "#BF2A0C",
+    pA100: "#FFB5A6",
+    pA200: "#FF8C76",
+    pA400: "#FF350D",
+    pA700: "#E52600",
+
+    // Secondary
+
+    s50: "#E3FDF2",
+    s100: "#BCF5DD",
+    s200: "#8EEDC6",
+    s300: "#6DE8B5",
+    s400: "#43E8A3",
+    s500: "#25E595",
+    s600: "#21D98C",
+    s700: "#1BC47E",
+    s800: "#17B272",
+    s900: "#0E8754",
+    sA100: "#B9F6DD",
+    sA200: "#76FFC7",
+    sA400: "#00E686",
+    sA700: "#00C875",
+
+    // Grays
+
+    g50: "#F7F8FA",
+    g100: "#EDF0F5",
+    g200: "#DDE3ED",
+    g300: "#CAD2E0",
+    g400: "#9DA9BD",
+    g500: "#69768C",
+    g600: "#525F75",
+    g700: "#444F61",
+    g800: "#333E50",
+    g900: "#181E26"
+  },
+  radius: 4,
+  font: "-apple-system, BlinkMacSystemFont, sans-serif",
+  monospace: '"SF Mono", "Roboto Mono", Menlo, monospace'
+};
