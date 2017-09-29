75cca9b3b30e83418f78c6fb12d0b5280c764283
Darin Dimitroff <d@thecrazyones.agency>
2017-09-29 21:46:27 +0300

    Use theme colors

--- src/components/extended/PrettyLink.js
+++ src/components/extended/PrettyLink.js
@@ -5,8 +5,13 @@ const PrettyLink = styled(Link)`
   min-width: 0 !important;
   text-decoration: none;
   font-weight: 500;
-  // Fix this
-  color: #ff4722;
+  color: ${props => props.theme.colors.pA400};
+  transition: all 250ms cubic-bezier(0.4, 0, 0.2, 1);
+  border-radius: 2px;
+  &:hover {
+    color: ${props => props.theme.colors.pA700};
+    background-color: ${props => props.theme.colors.p50};
+  }
 `
 
 export default PrettyLink
