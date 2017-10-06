6cc9b7794dfca958e13e9a496e344285b418a6cc
Darin Dimitroff <d@thecrazyones.agency>
2017-10-06 17:32:54 +0300

    Fix Activity width

--- src/pages/Activity.js
+++ src/pages/Activity.js
@@ -16,39 +16,37 @@ import Button from 'material-ui/Button'
 class Activity extends Component {
   render() {
     return (
-      <Measure mx="auto">
+      <Measure mx="auto" px={3}>
         <PageTitle title="Activity">
           <NotificationsIcon />
         </PageTitle>
-        <Container pt={2} is="main">
-          {/* Mark as read */}
-          <Flex justify="flex-end" mb={3}>
-            <Button raised>
-              <TypeIcon margin=".5em">
-                <DoneAllIcon />
-              </TypeIcon>Mark all as read
-            </Button>
-          </Flex>
-          {/* Unread */}
-          <Box mb={3}>
-            <Heading f={1} mb={2}>
-              Unread (2)
-            </Heading>
-          </Box>
-          <Flex mb={3} column>
-            {[0, 1, 2].map(value => (
-              <Box mb={2}>
-                <Notification />
-              </Box>
-            ))}
-          </Flex>
-          {/* Read */}
-          <Box mb={3}>
-            <Heading f={1} mb={2}>
-              Read
-            </Heading>
-          </Box>
-        </Container>
+        {/* Mark as read */}
+        <Flex justify="flex-end" mb={3}>
+          <Button raised>
+            <TypeIcon margin=".5em">
+              <DoneAllIcon />
+            </TypeIcon>Mark all as read
+          </Button>
+        </Flex>
+        {/* Unread */}
+        <Box mb={3}>
+          <Heading f={1} mb={2}>
+            Unread (2)
+          </Heading>
+        </Box>
+        <Flex mb={3} column>
+          {[0, 1, 2].map(value => (
+            <Box mb={2}>
+              <Notification />
+            </Box>
+          ))}
+        </Flex>
+        {/* Read */}
+        <Box mb={3}>
+          <Heading f={1} mb={2}>
+            Read
+          </Heading>
+        </Box>
       </Measure>
     )
   }
