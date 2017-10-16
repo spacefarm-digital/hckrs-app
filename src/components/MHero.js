c8f8d30b18fc417dd092abab174d0c2741d2eeca
Darin Dimitroff <d@thecrazyones.agency>
2017-10-16 17:42:23 +0300

    Make hero full width

--- src/components/MHero.js
+++ src/components/MHero.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Isvg from 'react-inlinesvg'
-import heroSVG from '../assets/testSVG/welcome.svg'
+import heroSVG from '../assets/testSVG/what-is-hckrs.svg'
 
 import { Heading, Text, Measure, Image, Flex, Box } from 'rebass'
 
@@ -10,31 +10,33 @@ import Grid from 'material-ui/Grid'
 import Button from 'material-ui/Button'
 
 const MHero = props => (
-  <Box bg="white" pt={[4, 4, 5]} is="section">
+  <Box bg="#fff" pt={[4, 4, 5]} is="section">
     <FluidContainer mx={-3} gutterless>
       <Grid container align="center" spacing={0}>
-        <Grid item xs={12} md={7}>
-          <Box px={3} mb={3}>
-            <Isvg src={heroSVG} />
-          </Box>
-        </Grid>
-        <Grid item xs={12} md={5}>
+        <Grid item xs={12} xl={5}>
           <Flex column align="center">
             <Box mb={[4, 4, 5]} px={3}>
-              <Heading f={[3, 4, 4, 5]} mb={2}>
+              <Heading f={[4, 4, 4, 5]} mb={2} color="p600">
                 Welcome to Hckrs
               </Heading>
-              <Measure mb={4}>
+              <Measure mb={3}>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
                 commodi distinctio facilis atque magnam ut reprehenderit, porro
                 eius iure nemo est tenetur sed officia officiis eaque eum
                 deserunt rerum itaque!
               </Measure>
-              <Button raised color="accent" href="/signup">
-                Let's get started!
-              </Button>
+              <Flex justify="center">
+                <Button raised color="accent" href="/signup">
+                  Let's get started!
+                </Button>
+              </Flex>
             </Box>
           </Flex>
+        </Grid>
+        <Grid item xs={12} xl={7}>
+          <Box px={3} mx="auto" style={{ maxWidth: 1024 }}>
+            <Isvg src={heroSVG} style={{ display: 'block' }} />
+          </Box>
         </Grid>
       </Grid>
     </FluidContainer>
