9b6d5c88ba80aaf026833daa5a9559cab6160535
Darin Dimitroff <d@thecrazyones.agency>
2017-10-30 20:16:37 +0200

    Add 404 animation

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,21 +1,48 @@
 import React, { Component } from 'react'
 import Isvg from 'react-inlinesvg'
+import styled, { keyframes } from 'styled-components'
 import { Link } from 'react-router-dom'
 
 import FluidContainer from '../components/extended/FluidContainer'
 
 import { Heading, Text, Measure, Flex, Box } from 'rebass'
 
-import NotFoundSVG from '../assets/testSVG/404.svg'
+import NotFoundSVG from '../assets/404.svg'
 
 import Button from 'material-ui/Button'
+
+const rotate360 = keyframes`
+	0% {
+    transform: rotate(0deg) scale(1);
+  }
+  
+  50% {
+    transform: rotate(180deg) scale(.9);
+  }
+
+	100% {
+    transform: rotate(360deg) scale(1);
+	}
+`
+
+const NotFoundIllustration = styled(Isvg)`
+  #js-animate-hole {
+    transform-origin: 50% 50%;
+    display: block;
+    animation: ${rotate360} 40s linear infinite;
+    animation-fill-mode: forwards;
+  }
+`
 
 class NotFound extends Component {
   render() {
     return (
-      <FluidContainer pt={5} tight>
+      <FluidContainer pt={3} tight>
+        <Box style={{ maxWidth: 550 }} mx="auto" mb={1}>
+          <NotFoundIllustration src={NotFoundSVG} uniquifyIDs={false} />
+        </Box>
         <Measure mx="auto">
-          <Heading f={4} mb={3} center>
+          <Heading f={4} mb={2} center>
             Ooops!
           </Heading>
           <Text>
@@ -24,11 +51,8 @@ class NotFound extends Component {
             page may have been moved.
           </Text>
         </Measure>
-        <Box style={{ maxWidth: 600 }} mx="auto" mb={3}>
-          <Isvg src={NotFoundSVG} />
-        </Box>
         <Flex justify="center">
-          <Button raised color="primary" component={Link} to="/MHome">
+          <Button raised color="primary" component={Link} to="/">
             Go back home
           </Button>
         </Flex>
